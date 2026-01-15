867128a4028f395b911424387cf016e8327f71e6
Peter Marigold <77348014+petermarigold@users.noreply.github.com>
2026-01-15 18:02:12 +0000

    Create create-checkout.php

---
+++ api/create-checkout.php
@@ -0,0 +1,52 @@
+<?php
+
+// Load Stripe library
+require_once __DIR__ . '/../vendor/autoload.php';
+
+\Stripe\Stripe::setApiKey('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');
+
+// Set CORS headers to allow your website to call this
+header('Access-Control-Allow-Origin: https://www.formcard.com');
+header('Access-Control-Allow-Methods: POST, OPTIONS');
+header('Access-Control-Allow-Headers: Content-Type');
+header('Content-Type: application/json');
+
+// Handle preflight OPTIONS request
+if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
+    http_response_code(200);
+    exit();
+}
+
+try {
+    $input = json_decode(file_get_contents('php://input'), true);
+    
+    $session = \Stripe\Checkout\Session::create([
+        'payment_method_types' => ['card'],
+        'line_items' => [[
+            'price_data' => [
+                'currency' => 'gbp',
+                'product_data' => [
+                    'name' => 'FORMcard Custom Colour Pack',
+                    'description' => $input['pack_count'] . ' packs (' . ($input['pack_count'] * 3) . ' cards)',
+                ],
+                'unit_amount' => $input['amount'], // Amount in pence
+            ],
+            'quantity' => 1,
+        ]],
+        'mode' => 'payment',
+        'success_url' => 'https://www.formcard.com/success.html?session_id={CHECKOUT_SESSION_ID}',
+        'cancel_url' => 'https://www.formcard.com/shop-test-stripe',
+        'metadata' => [
+            'order_details' => $input['order_details'],
+            'promo_code' => $input['promo_code'] ?? '',
+            'full_price' => $input['full_price'],
+            'discount' => $input['discount'],
+        ],
+    ]);
+
+    echo json_encode(['id' => $session->id]);
+} catch (Exception $e) {
+    http_response_code(500);
+    echo json_encode(['error' => $e->getMessage()]);
+}
+?>
